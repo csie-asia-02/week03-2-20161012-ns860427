2e3b0d4ea82b40fa962918dbc52e67685339fbdd
ns860427 <105021001@live.asia.edu.tw>
2016-10-12 11:53:26 +0800

    Homework1012-1

--- src/ex/ex01_1040210XX.java
+++ src/ex/ex01_1040210XX.java
@@ -5,11 +5,29 @@
  * Author: 1040210XX 周永振老師
  */
 
+import java.util.Scanner;
+
 public class ex01_1040210XX {
 
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int a=1 ; a>0 ; a++){
+		scanner = new Scanner(System.in); 
+		System.out.print("請輸入數字:"); 
+		String input = scanner.nextLine(); 
+		while(!input.matches("[0-9]*")){ 
+		System.out.printf("你輸入的%s不是數字，",input); 
+		System.out.print("請重新輸入數字:"); 
+		input = scanner.nextLine(); 
+		} 
+		int sum = 0; 
+		char[] ch_array = input.toCharArray();
+		for(int i=0;i<ch_array.length;i++){ 
+		int c = ch_array[i]-'0'; 
+		sum+=c;
+		} 
+		System.out.println(sum); 
+		}
 	}
-
-}
+	}
--- src/hw/hw04_1040210XX.java
+++ src/hw/hw04_1040210XX.java
@@ -1,10 +1,10 @@
 package hw;
 /*
- * Topic: 讓使用者輸入一正整數n，計算1*(1+1) + 2*(2+1) + 3*(3+1) + … + n*(n+1)並輸出結果
+ * Topic: 霈蝙��撓�銝�甇���n嚗���1*(1+1) + 2*(2+1) + 3*(3+1) + �� + n*(n+1)銝西撓�蝯��
  * Date: 2016/10/12
- * Author: 1040210XX 周永振老師
+ * Author: 1040210XX �瘞豢�葦
  */
-public class hw01_1040210XX {
+public class hw04_1040210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
